bc6bf0c4fb16789737876dea24ad8f073fb01440
wgreunke <wgreunke@gmail.com>
2025-02-28 15:21:53 -0800

    Changed to products url

--- app/products/[card_id]/page.tsx
+++ app/products/[card_id]/page.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 export default async function Cards({
    params, 
 }: {  
-     params: Promise<{ cardid:string}>;
+     params: Promise<{ card_id:string}>;
 }) {
-     const cardid = (await params).cardid;
+     const card_id = (await params).card_id;
  
      return (
     <div>
       <h2>cards</h2>
-      <p> {cardid}</p>
+      <p> {card_id}</p>
     </div>
      );
 }
