1e1eddbc4c3ba66931a0e69e7e635c901f2a29c9
wgreunke <wgreunke@gmail.com>
2025-03-21 10:55:14 -0700

    Fixing test page

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,13 +1,9 @@
 //This page is used to test db connection.  
 import { createClient } from '../../utils/supabase/server'
 import Link from 'next/link'
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-
-
 
 export default async function TestPage() {
-  const supabase = createClient(supabaseUrl, supabaseKey)
+  const supabase = createClient()
   
   const { data: parents, error } = await supabase
     .from('child_cards')
