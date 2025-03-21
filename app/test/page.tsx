546b28a1e6df6b26cef1b00a6c27d8fe1724a400
wgreunke <wgreunke@gmail.com>
2025-03-21 10:30:25 -0700

    Error on test page

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -2,30 +2,14 @@
 import { createClient } from '../../utils/supabase/server'
 import Link from 'next/link'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-
-
-
-
-//Connect to supabase using rest api
-const response = await fetch(`${supabaseUrl}/rest/v1/cards`)
-const data = await response.json()
-
-
-//https://supabase.com/docs/reference/javascript/update
-const { data: parents, error } = await supabase
-  .from('child_cards')
-  .select('*')
-  .eq('parent_card', 'Milwaukee_123')
- 
-  
-
-console.log(data)
-
 export default async function TestPage() {
+  const supabase = createClient();
+  
+  const { data: parents, error } = await supabase
+    .from('child_cards')
+    .select('*')
+    .eq('parent_card', 'Milwaukee_123');
+
   if (error) {
     return <div>Error loading parents: {error.message}</div>;
   }
