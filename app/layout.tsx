9e8dd99b59c53a6239e37488de01391309c526f7
wgreunke <wgreunke@gmail.com>
2025-03-21 11:35:32 -0700

    Adding analytics tag to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react"
+//https://vercel.com/docs/analytics/quickstart#add-the-analytics-component-to-your-app
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +32,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
