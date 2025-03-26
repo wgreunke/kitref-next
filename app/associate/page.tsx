847b4d5a74fc9aee844237d5963908ea096f5f2f
wgreunke <wgreunke@gmail.com>
2025-03-26 13:24:24 -0700

    Added associate page

---
+++ app/associate/page.tsx
@@ -0,0 +1,6 @@
+//This page is for associating existing cards with a new card.
+//It will take in a card_id and then display all the cards that are not associated with it.
+
+export default function AssociateCards() {
+    return(<div><p>Associate Cards</p></div>)
+}
