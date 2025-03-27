0f37829c4242825aba11f8e3151b175d41617b5b
wgreunke <wgreunke@gmail.com>
2025-03-26 21:52:02 -0700

    Going back to client side for now.  Will need to revist in future.

--- app/associate/page.tsx
+++ app/associate/page.tsx
@@ -1,26 +1,29 @@
 //This page is for associating existing cards with a new card.
 //It will take in a card_id and then display all the cards that are not associated with it.
 //This page will have to work on the client side
-'use client'
+//'use client'
 //This is hack, load all the cards on the server side and then have a serch to narrow down the card.
 //This is a hack because it loads all cards on the server side.
-
-
-
+'use client'
 import { useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export default function AssociateCards() {
-    //Grab the card_id from the url
-    const card_id = useSearchParams().get('card_id');
+  const searchParams = useSearchParams();
+  const card_id = searchParams.get('card_id');
+  const [data, setData] = useState(null);
 
-    //Get all the cards 
+  useEffect(() => {
+    async function fetchData() {
+      // Your async operations here
+    }
+    fetchData();
+  }, [card_id]);
 
-
-    
-    return(
+  return (
     <div>
-        <p>Associate Cards</p>
-        <p>Card Id: {card_id}</p>
-    </div>)
-
+      <p>Associate Cards</p>
+      <p>Card Id: {card_id}</p>
+    </div>
+  );
 }
