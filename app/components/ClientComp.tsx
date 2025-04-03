3e4799e847a0917f5d9574849d98d77b33b96196
wgreunke <wgreunke@gmail.com>
2025-04-03 15:03:02 -0700

    Testing component

---
+++ app/components/ClientComp.tsx
@@ -0,0 +1,5 @@
+
+export default function ClientComp() {
+  return <div>ClientComp</div>;
+}
+
---
+++ app/runcomp/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import ClientComp from "../components/ClientComp"
+
+export default function RunComp() {
+  return (
+    <div>
+      <ClientComp />
+    </div>
+  );
+}
