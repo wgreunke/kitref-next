b53955a892577aa09387decd50ec810258a32e1b
wgreunke <wgreunke@gmail.com>
2025-04-13 11:12:49 -0700

    Using simple reddit parser that just strips out the first text before the script.  No need to parse evertyhing else.

---
+++ app/components/RedditSimpleComponent.jsx
@@ -0,0 +1,25 @@
+//This is a simple component that just strips out the javascipt from embeeded code.
+
+export default function RedditSimpleComponent()
+{
+
+const sampleEmbededLink = `<blockquote class="reddit-embed-bq" style="height:500px" data-embed-height="740"><a href="https://www.reddit.com/r/MilwaukeeTool/comments/1ju3sk4/new_packout_bit_organizer/">New Packout Bit Organizer!</a><br> by<a href="https://www.reddit.com/user/Minute_Brilliant2796/">u/Minute_Brilliant2796</a> in<a href="https://www.reddit.com/r/MilwaukeeTool/">MilwaukeeTool</a></blockquote><script async="" src="https://embed.reddit.com/widgets.js" charset="UTF-8"></script>`
+
+function TextBeforeScript(embed: string)
+{
+    const textBeforeScript = embed.split('<script')[0];
+    return textBeforeScript;
+}
+
+
+
+    return (
+        <div>
+            <h1>Reddit Simple Component</h1>
+            <h2>{TextBeforeScript(sampleEmbededLink)}</h2>
+        </div>
+    )
+}
+
+
+
--- app/components/RedditViewer.tsx
+++ app/components/RedditViewer.tsx
@@ -6,39 +6,53 @@
 export default function RedditViewer()
 {
  const sampleEmbededLink = `<blockquote class="reddit-embed-bq" style="height:500px" data-embed-height="740"><a href="https://www.reddit.com/r/MilwaukeeTool/comments/1ju3sk4/new_packout_bit_organizer/">New Packout Bit Organizer!</a><br> by<a href="https://www.reddit.com/user/Minute_Brilliant2796/">u/Minute_Brilliant2796</a> in<a href="https://www.reddit.com/r/MilwaukeeTool/">MilwaukeeTool</a></blockquote><script async="" src="https://embed.reddit.com/widgets.js" charset="UTF-8"></script>`
-    function getRedditTitle(link: string)
-    {
-        const title = link.split('/')[4];
-        return title;
-    }
 
-    function getRedditAuthor(link: string)
+ 
+    function getRedditAuthor(embed: string)
     {
-        const author = link.split('/')[5];
+        const author = embed.split('/')[5];
         return author;
     }
 
-    function getRedditBaseLink(link: string)
+    function getRedditBaseLink(embed: string)
     {
-        const baseLink = link.split('/')[0];
+        const baseLink = embed.split('/')[0];
         return baseLink;
     }
 
 
-    function getRedditSubreddit(link: string)
+    function getRedditSubreddit(embed: string)
     {
-        const subreddit = link.split('/')[3];
+        const subreddit = embed.split('/')[3];
         return subreddit;
     }
 
- return (
-        <div>
-            <h1>Reddit Viewer</h1>
-            <h2>{getRedditTitle(sampleEmbededLink)}</h2>
-            <h3>{getRedditAuthor(sampleEmbededLink)}</h3>
-            <h4>{getRedditBaseLink(sampleEmbededLink)}</h4>
-            
-        </div>
-    )
+
+    function getReddit(embed: string)
+    {
+        const reddit = embed.split('/')[3];
+        return reddit;
+    }
+
+    function getRedditTitle(embed: string) {
+        // Use regex to find the first <a href="...">...</a> pattern
+        const linkRegex = /<a href="[^"]+"[^>]*>[^<]+<\/a>/;
+        const match = embed.match(linkRegex);
+        
+        if (match) {
+            return match[0]; // Return the complete <a> tag
+        }
+        
+        return "No link found";
+    }
+
+   return (
+          <div>
+              <h1>Reddit Viewer</h1>
+              <h2>{getRedditTitle(sampleEmbededLink)}</h2>
+              <h3>{getRedditAuthor(sampleEmbededLink)}</h3>
+              <h4>{getRedditBaseLink(sampleEmbededLink)}</h4>
+          </div>
+      )
 }
 
