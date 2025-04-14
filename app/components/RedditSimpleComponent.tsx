c62f0ec2ee1250fa8daa02e1138f6bf1a628650f
wgreunke <wgreunke@gmail.com>
2025-04-14 16:31:15 -0700

    Trying to fix no loading on phone.

--- app/components/RedditSimpleComponent.tsx
+++ app/components/RedditSimpleComponent.tsx
@@ -1,38 +1,38 @@
-//This is a simple component that just strips out the javascipt from embeeded code.
-
+'use client'
+import { useEffect, useRef } from 'react'
 import Script from 'next/script'
-//Note to prevent cross-site scripting attacks, 
-//need to add some security measures. like Dompurify.
+import DOMPurify from 'dompurify'
 
-//Need to add error checking if the embeded link is not valid.
+export default function RedditSimpleComponent({ RedditEmbededLink }: { RedditEmbededLink?: string }) {
+    const containerRef = useRef<HTMLDivElement>(null);
 
-
-
-
-export default function RedditSimpleComponent({ RedditEmbededLink }: { RedditEmbededLink?: string })
-{
-    // Use the nullish coalescing operator
     const testLink = `<blockquote class="reddit-embed-bq" style="height:500px" data-embed-height="740"><a href="https://www.reddit.com/r/MilwaukeeTool/comments/1ju3sk4/new_packout_bit_organizer/">New Packout Bit Organizer!</a><br> by<a href="https://www.reddit.com/user/Minute_Brilliant2796/">u/Minute_Brilliant2796</a> in<a href="https://www.reddit.com/r/MilwaukeeTool/">MilwaukeeTool</a></blockquote><script async="" src="https://embed.reddit.com/widgets.js" charset="UTF-8"></script>`;
 
     const embeddedLink = RedditEmbededLink ?? testLink;
 
-    function TextBeforeScript(embed: string)
-    {
-        const textBeforeScript = embed.split('<script')[0];
-        return textBeforeScript;
+    function TextBeforeScript(embed: string) {
+        return embed.split('<script')[0];
     }
+
+    useEffect(() => {
+        // Trigger Reddit embed reload after mount
+        if (window?.__REDDIT_EMBED__?.load) {
+            window.__REDDIT_EMBED__.load();
+        }
+    }, [embeddedLink]);
 
     return (
         <div>
-            <div 
+            <div
+                ref={containerRef}
                 style={{ width: '300px', margin: '0 auto' }}
                 dangerouslySetInnerHTML={{ __html: TextBeforeScript(embeddedLink) }}
             />
-            <Script 
+            <Script
                 src="https://embed.reddit.com/widgets.js"
                 strategy="afterInteractive"
                 async
             />
         </div>
-    )
+    );
 }
