3050bf2b7f7df899b19535dcf7797a9f8d39429a
wgreunke <wgreunke@gmail.com>
2025-04-21 10:25:49 -0700

    Added google anlaytics.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/Footer";
 import { Analytics } from "@vercel/analytics/react"
+import { GoogleAnalytics } from "@next/third-parties/google"
 //https://vercel.com/docs/analytics/quickstart#add-the-analytics-component-to-your-app
 
  
@@ -31,6 +32,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <GoogleAnalytics gaId="G-F3TSQ2Z8NM" />
         {children}
         <Analytics />
         <Footer />
